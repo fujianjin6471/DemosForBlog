b4fbdc89d75b2d29bc8608b8e9e4716a0e9d7786
fujianjin6471 <fujianjin6471@yeah.net>
2015-04-23 11:31:02 +0800

    Add Page Control

--- UIScrollViewInStoryboard/ViewController.swift
+++ UIScrollViewInStoryboard/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        someImprovement()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pageTurn(sender: UIPageControl) {
+        let viewSize = scrollView.frame.size
+        let rect = CGRectMake(CGFloat(sender.currentPage) * viewSize.width, 0, viewSize.width, viewSize.height)
+        scrollView.scrollRectToVisible(rect, animated: true)
+    }
 
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset
+        let bounds = scrollView.frame
+        pageControl.currentPage = Int(offset.x / bounds.size.width)
+    }
+    
+    func someImprovement() {
+        scrollView.pagingEnabled = true
+        scrollView.bounces = false
+        scrollView.showsHorizontalScrollIndicator = false
+    }
 }
 
