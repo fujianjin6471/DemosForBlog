d998f2ada610684536ad2fc43daaf7cc309b326a
fujianjin6471 <fujianjin6471@yeah.net>
2015-06-05 09:25:38 +0800

    Initial Commit

---
+++ WhichLayoutSubviews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhichLayoutSubviews
+//
+//  Created by 付剑津 on 6/5/15.
+//  Copyright (c) 2015 fujianjin6471. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
