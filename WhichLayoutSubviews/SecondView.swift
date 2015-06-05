59c3549bc5ba4a9383333d601e6d84d718784133
fujianjin6471 <fujianjin6471@yeah.net>
2015-06-05 22:41:54 +0800

    Xcode 6.3.2

---
+++ WhichLayoutSubviews/SecondView.swift
@@ -0,0 +1,24 @@
+//
+//  SecondView.swift
+//  WhichLayoutSubviews
+//
+//  Created by 付剑津 on 6/5/15.
+//  Copyright (c) 2015 fujianjin6471. All rights reserved.
+//
+
+import UIKit
+
+class SecondView: UIView {
+
+    override func layoutSubviews() {
+        println("second layoutSubviews")
+    }
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- WhichLayoutSubviews/ViewController.swift
+++ WhichLayoutSubviews/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var thirdWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var secondThirdBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var secondThirdTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var secondLeftConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeConstraint(sender: AnyObject) {
+        thirdWidthConstraint.constant += 1
+//        secondThirdBottomConstraint.constant += 1
+//        secondThirdTopConstraint.constant += 1
+//        secondLeftConstraint.constant += 1
+    }
 
 }
 
