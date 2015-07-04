dca07255f8cca4a5da6664e3336f47bfa4101963
fujianjin6471 <fujianjin6471@yeah.net>
2015-07-04 16:38:25 +0800

    Initial Commit

---
+++ UsingNSCoder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsingNSCoder
+//
+//  Created by 付剑津 on 7/4/15.
+//  Copyright (c) 2015 fujianjin6471. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
